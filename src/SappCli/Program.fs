4f245e8c27ae48d1217b523a5395d346ef99c5d2
Christoph Pletz <christoph.pletz@trivadis.com>
2019-04-05 23:44:05 +0200

    Start with Cli

---
+++ src/SappCli/Program.fs
@@ -0,0 +1,26 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open Microsoft.WindowsAPICodePack.Dialogs
+open System
+
+let selectFlacFiles() =
+
+    use dlg = new CommonOpenFileDialog("Open FLAC MusicFIles")
+    dlg.DefaultExtension <- "FLAC"
+    dlg.Multiselect <- true
+    dlg.Filters.Add(CommonFileDialogFilter("FLAC Files", "flac"))
+
+
+    let dlgResult = dlg.ShowDialog()
+    if dlgResult = CommonFileDialogResult.Ok then
+        dlg.FileName
+    else
+        ""
+
+[<STAThread>]
+[<EntryPoint>]
+let main argv = 
+    let file = selectFlacFiles()
+    printfn "%s" file
+    0 // return an integer exit code
