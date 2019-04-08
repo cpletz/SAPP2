0ea6b68e5f3b451ab1fc689efc83680a46f51518
Christoph Pletz <christoph.pletz@trivadis.com>
2019-04-08 07:05:05 +0200

    start with cli

--- src/SappCli/Program.fs
+++ src/SappCli/Program.fs
@@ -1,26 +1,12 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿module Main
 
-open Microsoft.WindowsAPICodePack.Dialogs
 open System
-
-let selectFlacFiles() =
-
-    use dlg = new CommonOpenFileDialog("Open FLAC MusicFIles")
-    dlg.DefaultExtension <- "FLAC"
-    dlg.Multiselect <- true
-    dlg.Filters.Add(CommonFileDialogFilter("FLAC Files", "flac"))
-
-
-    let dlgResult = dlg.ShowDialog()
-    if dlgResult = CommonFileDialogResult.Ok then
-        dlg.FileName
-    else
-        ""
 
 [<STAThread>]
 [<EntryPoint>]
 let main argv = 
-    let file = selectFlacFiles()
-    printfn "%s" file
+    let config = Config.fromSettings()
+    match SelectFlacFiles.getFiles config.FLACRootPath with
+    | Some(files) -> (printf "%A" files)
+    | _ -> printf "None"
     0 // return an integer exit code
---
+++ src/SappCli/SelectFlacFiles.fs
@@ -0,0 +1,18 @@
+﻿module SelectFlacFiles
+
+open Microsoft.WindowsAPICodePack.Dialogs
+
+let getFiles rootPath =
+
+    use dlg = new CommonOpenFileDialog("Open FLAC Files")
+    dlg.DefaultExtension <- "FLAC"
+    dlg.Multiselect <- true
+    dlg.Filters.Add(CommonFileDialogFilter("FLAC Files", "flac"))
+    dlg.DefaultDirectory <- rootPath
+
+    if dlg.ShowDialog() = CommonFileDialogResult.Ok then
+        dlg.FileNames 
+        |> Seq.toList
+        |> Some
+    else
+        None
